761959a3e547442fef8f744759f293f0acdaff33
Nishant763 <bhatianishant763@gmail.com>
2022-03-31 21:12:27 +0530

    domain,ui fixes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -17,20 +17,20 @@ const Home = () => {
     }
     return (
         <div className={styles.cardWrapper}>
-            <Card title="Welcome to Codershouse!" icon="logo">
+            <Card title="Welcome to Voxs-Audio-Room!" icon="logo">
                 <p className={styles.text}>
-                    We’re working hard to get Codershouse ready for everyone!
-                    While we wrap up the finishing youches, we’re adding people
+                    We’re working hard to get Voxs-Audio-Room ready for everyone!
+                    While we wrap up the finishing touches, we’re adding people
                     gradually to make sure nothing breaks
                 </p>
                 <div>
                     <Button onClick={startRegister} text="Let's Go" />
                 </div>
-                <div className={styles.signinWrapper}>
+                {/* <div className={styles.signinWrapper}>
                     <span className={styles.hasInvite}>
                         Have an invite text?
                     </span>
-                </div>
+                </div> */}
             </Card>
         </div>
     );
